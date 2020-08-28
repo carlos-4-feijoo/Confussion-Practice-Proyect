ef99c3940b9ade4b7749703b6382fbd9bc6b4125
Carlos Feijoo <carlos.4.feijoo@gmail.com>
2020-08-28 15:44:37 -0500

    Before redux, forms

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from "reactstrap";
+import { Link } from "react-router-dom";
 
 function RenderDish({ dish }) {
   if (dish !== undefined) {
@@ -18,7 +19,7 @@ function RenderDish({ dish }) {
 }
 function RenderComments({ dish }) {
   if (dish !== undefined) {
-    const comments = dish.comments.map((comment) => {
+    const comments = dish.map((comment) => {
       return (
         <li>
           <p>{comment.comment}</p>
@@ -50,11 +51,23 @@ const DishDetail = (props) => {
     return (
       <div className="container">
         <div className="row">
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link to="/menu">Menu</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+          </Breadcrumb>
+          <div className="col-12">
+            <h3>{props.dish.name}</h3>
+            <hr />
+          </div>
+        </div>
+        <div className="row">
           <div className="col-12 col-sm-5 m-1">
             <RenderDish dish={props.dish} />
           </div>
           <div className="col-12 col-sm-5 m-1">
-            <RenderComments dish={props.dish} />
+            <RenderComments dish={props.comments} />
           </div>
         </div>
       </div>
